f01259d1ff1e3a28efd61dec8210cacf3096b7eb
Richard Hpa <richard.hpa@hotmail.com>
2020-01-15 10:53:33 +1300

    added in react preset and made a small react component

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-const $ = require('jquery');
+import React from 'react';
+import { render } from 'react-dom';
+import './css/style.css';
 
-$('#target').html('Hello World');
+const Greeting = () => <h1>Hello from React</h1>
+
+render(
+    <Greeting />,
+    document.getElementById('target')
+)
